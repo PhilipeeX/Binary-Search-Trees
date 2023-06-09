e3bc11c065b56db874865887136559b98417747d
PhilipeeX <dev.philipe@outlook.com>
2023-06-09 18:54:17 -0300

    tentativa falha de debugar usando os pontos de interrupçao do vscode com arquivo json

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -1,3 +1,4 @@
+require 'pry-byebug'
 class Node
     def initialize(value)
         @data = value
@@ -21,15 +22,26 @@ end
 class Tree
     def initialize(array)
         @array = array
+        @root = build_tree
     end
 
     def build_tree
         @array = sort_array
+        i = 0
+        while i < @array.length
+            @array[i] = Node.new(@array[i])
+            i += 1
+        end
+        @array[@array.length / 2]
     end
 
     # sort and remove duplicates!
     def sort_array
         @array = @array.sort
-        @array.uniq!
+        @array.uniq
     end
 end
+
+tree1 = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+tree1
