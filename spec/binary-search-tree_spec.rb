deded9a96c406534a6f6eef804c20717ae56c8e2
PhilipeeX <dev.philipe@outlook.com>
2023-06-10 11:53:41 -0300

    Iniciada a configuracao do rspec para realizar testes

---
+++ spec/binary-search-tree_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../binary-search-tree.rb'
+
+describe Tree do
+  it 'faz algo' do
+    arvore = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+    expect(arvore)
+  end
+end
